9c578d592dac3915768455269d4f5f0d2d993511
CodeLuminary <victorijoni@yahoo.com>
2022-07-15 00:38:58 +0100

    Updated

--- backend/UsersService/server.js
+++ backend/UsersService/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const https = require('https')
+//const fetch = require("node-fetch");
+const axios = require('axios')
 
 const app = express();
 
@@ -11,10 +14,35 @@ app.use(cors(
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
-app.get('/',(req,res)=>{
+app.get('/users',(req,res)=>{
     res.send({
         isSuccessful: true,
         message: 'I worked'
+    })
+})
+
+app.get('/users/yes',(req,res)=>{
+    res.send({
+        isSuccessful:true,
+        message: 'yess'
+    })
+})
+
+app.get('/users/trans',(req,res)=>{
+    /*fetch("http://transaction:3000/")
+    .then(result=>result.json)
+    .then(result=>res.send(result))*/
+    /*https.request({
+        hostname: 'http://transaction:3000',
+        method: 'GET'
+    }, result=>{
+        result.on('data', d=>{
+            res.send(d)
+        })
+    })*/
+    axios.get('http://transaction:3000')
+    .then(response=>{
+        res.send(response.data)
     })
 })
 
@@ -25,7 +53,7 @@ app.all('/*',(req,res)=>{
      //res.sendFile(path.join(__dirname,'static/error-page.html'))  
  })
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 app.listen(port, () =>{
     console.log(`Server ${process.pid} started on port ${port}`)
 });
