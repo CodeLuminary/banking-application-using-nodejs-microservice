be7222a37a50a6b904c9b78f736e3f56406441cd
CodeLuminary <victorijoni@yahoo.com>
2022-08-13 23:59:53 +0100

    Project refactored

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="dashboard">
+
   </div>
 </template>
 
-<style>
+<style scoped>
 @media (min-width: 1024px) {
-  .about {
+  .dashboard {
     min-height: 100vh;
     display: flex;
     align-items: center;
