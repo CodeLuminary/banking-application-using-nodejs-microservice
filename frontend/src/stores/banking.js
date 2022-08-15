b98ae6e0e386772b5f79a689d3aba712351320b0
CodeLuminary <victorijoni@yahoo.com>
2022-08-15 04:36:11 +0100

    users actions added

---
+++ frontend/src/stores/banking.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia'
+
+export const useBankingStore = defineStore({
+  id: 'bank',
+  state: () => ({
+    users: {}
+    transactions: {}
+  }),
+  getters: {
+    doubleCount: (state) => state.counter * 2
+  },
+  actions: {
+    increment() {
+      this.counter++
+    },
+
+    async getAllUsers(){
+      return this.users;
+    },
+    async getUserTransactions(){
+      try{
+        const response = await fetch('/users')
+        const data = await response.json();
+        if(data.isSuccessful) this.users = data.users;
+        else console.log(data)
+      }
+      catch(error){
+        console.log(error)
+      }
+    }
+  }
+})
