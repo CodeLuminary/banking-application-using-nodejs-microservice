e4afe92a2a6ab184c858a9f1b551ffb4a76d301d
CodeLuminary <victorijoni@yahoo.com>
2022-08-16 14:08:12 +0100

    Updated

--- frontend/src/components/HelloUser.vue
+++ frontend/src/components/HelloUser.vue
@@ -22,18 +22,12 @@ h1 {
   top: -10px;
 }
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
+.greetings h1{
   text-align: center;
 }
 
 @media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
+  .greetings h1{
     text-align: left;
   }
 }
