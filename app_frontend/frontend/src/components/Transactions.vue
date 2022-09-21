040c3653093de253f9b842bc3e1dbb9dd4a3bb62
CodeLuminary <victorijoni@yahoo.com>
2022-09-21 04:16:47 +0100

    Transactions added

---
+++ app_frontend/frontend/src/components/Transactions.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import {useBankingStore} from '@/stores/banking';
+    const banking = useBankingStore();
+</script>
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Amount</th>
+                    <th>Status</th>
+                    <th>Date/Time</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in banking.transactions">
+                    <td>{{user.from}}</td>
+                    <td>{{user.to}</td>
+                    <td>{{user.amount}}</td>
+                    <td>{{user.isSuccessful ? `Successful`: ``}}</td>
+                    <td>{{user.timestamp}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
